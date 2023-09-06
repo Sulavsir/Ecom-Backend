c4a7f8544ba6cdcaa27800799158d6d18e918f22
Roshan Bist <roshanbista0123@gmail.com>
2023-09-06 22:54:21 +0545

    updated

---
+++ models/customer/customerModel.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+
+const customerModel = new mongoose.Schema({
+    name :{
+        type: String,
+        required: true
+    },
+    email:{
+        type:String,
+        required:[true,"Email is compulsary"],
+        match:[/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,"Your email doesnot match the format"],
+        unique:true,    
+    },
+    verified:{
+        type: Boolean, 
+        required:[true,"It is necessary to mention if the user is verified or not"],
+        default:false
+    },
+    password:{
+        type:String,
+        required:[true,"Password is compulsary"],
+        match:[/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/,"Please provide a valid password"]
+    },
+    photo: String
+},{timestamps: true})
+
+module.exports= mongoose.model("Customer-model",customerModel);
