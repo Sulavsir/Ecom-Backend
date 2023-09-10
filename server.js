ab698d5de73481775ac069e535682f2775be9c06
Roshan Bist <roshanbista0123@gmail.com>
2023-09-10 17:31:04 +0545

    updated

--- server.js
+++ server.js
@@ -5,13 +5,7 @@ const mongoose = require("mongoose");
 const customerAPI = require("./routes/customerRoutes/customerRoutes");
 const SuperAdminAPI = require("./routes/SuperAdminRoutes/SuperAdminRoutes");
 const AdminAPI = require("./routes/AdminRoutes/AdminRoutes");
-<<<<<<< HEAD
-const subscriptionAPI  = require("./routes/subscriptionRoutes/subscriptionRoutes");
-const subscriptionKhalti = require("./routes/khaltiPayment/khaltipayment");
-
-=======
 const productAPI = require('./controller/products/productRoutes')
->>>>>>> 6f29cb5746e66bbfae102e49be8faa9504b7a108
 
 
 const cors = require("cors");
